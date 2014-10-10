7b97163fddfab75a5cdfc357b94c9aacd6c199a2
gabylopez <lop13056@uvg.edu.gt>
2014-10-10 16:07:20 -0600

    Update SplayTreeSet.java

--- src/javaapplication3/SplayTreeSet.java
+++ src/javaapplication3/SplayTreeSet.java
@@ -1,3 +1,12 @@
+/*
+ * SplayTreeSet
+ * 10 de Octubre del 2014
+ * María Isabel Fernández 13024
+ * Ana Bartra 13643
+ * Andrés Lainfiesta 13072
+ * Maria Gabriela López 13056
+ */
+ 
 class SplayTreeSet implements WordSet
 {
 	private SplayTree<Word> base;
