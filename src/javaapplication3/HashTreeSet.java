72411fcd555ae8dc97e169b98a0cb09c7a5b30db
gabylopez <lop13056@uvg.edu.gt>
2014-10-10 16:01:25 -0600

    Update HashTreeSet.java

--- src/javaapplication3/HashTreeSet.java
+++ src/javaapplication3/HashTreeSet.java
@@ -1,7 +1,12 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * HashTreeSet
+ * 10 de Octubre del 2014
+ * María Isabel Fernández 13024
+ * Ana Bartra 13643
+ * Andrés Lainfiesta 13072
+ * Maria Gabriela López 13056
  */
+ 
 import java.util.HashMap;
 
 public class HashTreeSet implements WordSet{
